26b91c060a8175c20f6f8bfb59ef22b40f06d62b
phung.nc <phung.nc@aris-vn.com>
2015-06-27 11:35:22 +0700

    init jest demo

---
+++ jest/member.jsx
@@ -0,0 +1,58 @@
+var React   = require('react/addons'),
+    request = require('superagent'),
+    Router  = require('react-router'),
+    mui     = require('material-ui')
+    ;
+
+var Route         = Router.Route;
+var RouteHandler  = Router.RouteHandler;
+var DefaultRoute  = Router.DefaultRoute;
+var Link          = Router.Link;
+
+var ThemeManager = new mui.Styles.ThemeManager();
+
+var ListItem     = mui.ListItem;
+var Avatar       = mui.Avatar;
+var IconButton   = mui.IconButton;
+var ToggleStar   = require('./toggle-star.jsx');
+var Delete       = require('./delete.jsx');
+var Colors       = mui.Styles.Colors ;
+    
+var Member = React.createClass({
+  childContextTypes: {
+    muiTheme: React.PropTypes.object
+  },
+  getInitialState() {
+    return {
+      liked: this.props.initialLike
+    };
+  },
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  },
+  onClick() {
+    this.setState({liked: !this.state.liked});
+  },
+  _onDelete() {
+    this.props.onDelete(this.props.id);
+  },
+  render() {
+    var starColor = this.state.liked ? Colors.yellow800 : Colors.grey400; 
+    return (
+        <ListItem 
+          leftAvatar={<Avatar src={this.props.src} />}
+          rightIconButton={
+            <div>
+              <IconButton onClick={this.onClick}><ToggleStar color={starColor} /></IconButton>
+              <IconButton onClick={this._onDelete}><Delete color={Colors.red800} /></IconButton>
+            </div>}
+          secondaryTextLines={2}> 
+          {this.props.name} 
+        </ListItem>
+      );
+  }
+});
+
+module.exports = Member;
